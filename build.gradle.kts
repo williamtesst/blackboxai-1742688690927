49ae1b5be7337099051deb0e830e2f36b2b202d2
williamtesst <tongbithu6@gmail.com>
2025-03-23 08:23:47 +0700

    Add build.gradle.kts 1742693027466

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform") version "1.9.0"
     id("org.jetbrains.compose") version "1.5.0"
+    id("com.android.application") version "8.0.0"
 }
 
 repositories {
@@ -10,6 +11,14 @@ repositories {
 }
 
 kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "11"
+            }
+        }
+    }
+    
     jvm {
         compilations.all {
             kotlinOptions.jvmTarget = "11"
@@ -29,11 +38,58 @@ kotlin {
             }
         }
         
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.activity:activity-compose:1.7.0")
+                implementation("androidx.appcompat:appcompat:1.6.1")
+                implementation("androidx.core:core-ktx:1.10.0")
+            }
+        }
+        
         val jvmMain by getting {
             dependencies {
                 implementation(compose.desktop.currentOs)
             }
         }
+    }
+}
+
+android {
+    namespace = "com.eoswallet"
+    compileSdk = 34
+    
+    defaultConfig {
+        applicationId = "com.eoswallet"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.0"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
