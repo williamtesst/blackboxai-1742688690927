6bfaf047fb3ceae7e1efdc4415deb563cffe8365
williamtesst <tongbithu6@gmail.com>
2025-03-23 08:23:43 +0700

    Add src/androidMain/kotlin/com/eoswallet/MainActivity.kt 1742693023242

---
+++ src/androidMain/kotlin/com/eoswallet/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.eoswallet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import App
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            App()
+        }
+    }
+}
