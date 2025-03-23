15eb10103df169ee8809eb3bc14b139ad73bc093
williamtesst <tongbithu6@gmail.com>
2025-03-23 07:11:45 +0700

    Add settings.gradle.kts 1742688704505

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+    }
+}
+
+rootProject.name = "eos-wallet-kmp"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
